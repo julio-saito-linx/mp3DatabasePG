aa52532782c46073a6cc33861d518de52d6982f3
Julio Saito <saitodisse@gmail.com>
2012-09-08 12:40:06 -0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3DatabasePG::Application.config.secret_token = 'fd52d000646a0c5bd49044c1a1842d936dc2d2b282342fcf64058ac2ba67b35dfbece19971e9b27e2973ee39cc60cc6ebd2a4f6185b605775ce58cda10d8c6ec'
